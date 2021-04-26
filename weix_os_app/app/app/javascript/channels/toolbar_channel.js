d36416cad8d18b7ecaaa439259732a02a53adb15
Lukas Weixelbraun <mail@lukas-weixelbraun.com>
2021-04-26 21:45:34 +0200

    added system menue and log out function

--- weix_os_app/app/app/javascript/channels/toolbar_channel.js
+++ weix_os_app/app/app/javascript/channels/toolbar_channel.js
@@ -3,8 +3,32 @@ import moment from "moment"
 // --- Date Time for System Time ---
 
 window.getSystemTime = function() {
-  let now = moment().format('dd, DD. MMM HH:mm');
-  document.getElementById('system-time').textContent = now;
+  var systemTime = document.getElementById('system-time');
+
+  if(systemTime) {
+    let now = moment().format('dd, DD. MMM HH:mm');
+    systemTime.textContent = now;
+  }
 }
 
 setInterval(getSystemTime, 2000);
+
+window.logOut = function() {
+  $.ajax({
+    global: false,
+    type: "DELETE",
+    url: "/users/sign_out",
+    dataType: 'html',
+    success: function (html) {
+      console.log("Good Bye!");
+      Turbolinks.clearCache()
+      Turbolinks.visit("http://localhost:3000/", {"action":"replace"})
+      
+    }
+  });
+}
+
+window.openSystemMenue = function(e) {
+  e.preventDefault();
+  document.getElementById('system-menue-box').classList.toggle('hidden');
+}
